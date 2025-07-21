e62b434611b8f9f83ae67058b055d88eb44c948d
MINDHELL <Tgpurpose640@gmail.com>
2025-07-21 12:53:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,21 +17,21 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = os.getenv("API_ID", "")
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-MONGO_DB = os.getenv("MONGO_DB", "")
-OWNER_ID = list(map(int, os.getenv("OWNER_ID", "").split())) # list seperated via space
-DB_NAME = os.getenv("DB_NAME", "telegram_downloader")
+API_ID = os.getenv("API_ID", "27083483")
+API_HASH = os.getenv("API_HASH", "1ba790464745c13ce149649d73137e52")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8090992345:AAGsAHZ6JgoWArQQneQJnt6LUttoD9AEF-E")
+MONGO_DB = os.getenv("MONGO_DB", "mongodb+srv://aarshhub:6L1PAPikOnAIHIRA@cluster0.6shiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+OWNER_ID = list(map(int, os.getenv("OWNER_ID", "7246766108").split())) # list seperated via space
+DB_NAME = os.getenv("DB_NAME", "xsave")
 STRING = os.getenv("STRING", None) # optional
-LOG_GROUP = int(os.getenv("LOG_GROUP", "-1001234456")) # optional with -100
-FORCE_SUB = int(os.getenv("FORCE_SUB", "-10012345567")) # optional with -100
+LOG_GROUP = int(os.getenv("LOG_GROUP", "-1002596089232")) # optional with -100
+FORCE_SUB = int(os.getenv("FORCE_SUB", "-1002668202623")) # optional with -100
 MASTER_KEY = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq") # for session encryption
 IV_KEY = os.getenv("IV_KEY", "s7Yx5CpVmE3F") # for decryption
 YT_COOKIES = os.getenv("YT_COOKIES", YTUB_COOKIES)
 INSTA_COOKIES = os.getenv("INSTA_COOKIES", INST_COOKIES)
-FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
+FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "100"))
 PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "500"))
-JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/team_spy_pro") # this link for start command message
-ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/username_of_admin")
+JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/Allvidsbackup3") # this link for start command message
+ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/skyler004")
 
